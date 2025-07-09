cd2b566ed5dde1a27878e6e8820a1b845c3ed156
Sejal <sejal.thakur0124@gmail.com>
2025-07-09 20:10:17 +0530

    Create count_digits.cpp

---
+++ BasicMaths/count_digits.cpp
@@ -0,0 +1,18 @@
+// Problem : https://www.geeksforgeeks.org/problems/count-digits5716/1
+// Category: Basic Maths
+// Language: C++
+// Description: Given a positive integer n, count the number of digits in n that divide n evenly (i.e., without leaving a remainder). Return the total number of such digits.
+class Solution {
+  public:
+    // Function to count the number of digits in n that evenly divide n
+    int evenlyDivides(int n) {
+        // code here
+        int og_num=n, count=0;
+        while (n>0){
+            int last_digit=n%10;
+            if (last_digit!=0 && og_num%(n%10)==0) count++;
+            n=n/10;
+        }
+    return count;
+    }
+};
