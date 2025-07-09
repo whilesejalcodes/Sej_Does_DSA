5ebad2d9d25404d71f7f28befb282ffec88b8885
Sejal <sejal.thakur0124@gmail.com>
2025-07-10 00:48:16 +0530

    Create 06_prime_or_not.cpp

---
+++ BasicMaths/06_prime_or_not.cpp
@@ -0,0 +1,20 @@
+// Problem : https://www.geeksforgeeks.org/problems/check-prime/1
+// Description : Given an integer n check if n is prime or not. A prime number is a number that is divisible by 1 and itself only.
+// User function Template for C++
+
+bool prime(int n) {
+    // Write your code here
+    int count=0;
+    if (n==1) return false;
+    for (int i=1;i<=sqrt(n);i++){
+        if (n%i==0){
+            count++;
+            if ((n/i)!=i){
+                count++;
+            }
+        }
+    }
+    if (count>2) return false;
+    else return true;
+    // returns a boolean value
+}
