28bd7fcbf48adc3f1d6bb88a26a5d65422fbf0de
Sejal <sejal.thakur0124@gmail.com>
2025-11-07 22:44:17 +0530

    Update 06_reverse_an_array.cpp

--- 1.Learn The Basics/1.5. Basic Recursion/06_reverse_an_array.cpp
+++ 1.Learn The Basics/1.5. Basic Recursion/06_reverse_an_array.cpp
@@ -55,3 +55,26 @@ class Solution {
         
     }
 };
+//or using one variable
+#include <bits/stdc++.h>
+using namespace std;
+void reverseArray(int arr[], int i,int n){
+    if (i >= n/2) return;
+    swap(arr[i], arr[n-i-1]);
+    reverseArray(arr, i + 1, n);
+}
+int main() {
+    int n;
+    cout << "please enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    reverseArray(arr, 0, n);
+    cout << "The reversed array is: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
